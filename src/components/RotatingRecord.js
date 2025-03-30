81929ec7b010d0dce5631051172011251dfa56f2
nneoma <nneoma.amy@gmail.com>
2025-03-30 10:55:24 -0500

    go

--- src/components/RotatingRecord.js
+++ src/components/RotatingRecord.js
@@ -1,35 +1,3 @@
-/*
-import React, { useEffect, useState } from 'react';
-
-function RotatingRecord({ track }) {
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  useEffect(() => {
-    const handlePlay = () => setIsPlaying(true);
-    const handlePause = () => setIsPlaying(false);
-    const handleStop = () => setIsPlaying(false);
-
-    track.sound.on('play', handlePlay);
-    track.sound.on('pause', handlePause);
-    track.sound.on('stop', handleStop);
-
-    return () => {
-      track.sound.off('play', handlePlay);
-      track.sound.off('pause', handlePause);
-      track.sound.off('stop', handleStop);
-    };
-  }, [track.sound]);
-
-  return (
-    <div className={`record ${isPlaying ? 'spinning' : ''}`}>
-      <img src="/record.jpeg" alt="Record" />
-    </div>
-  );
-}
-
-export default RotatingRecord;
-*/
-
 import React, { useState, useEffect } from 'react';
 
 function RotatingRecord({ track }) {
