c337281b8fa8a210a1a670dedf8f4332e9e3f1ab
nneoma <nneoma.amy@gmail.com>
2025-03-30 09:29:08 -0500

    got jaimes crossfader

--- src/components/CrossFader.js
+++ src/components/CrossFader.js
@@ -1,30 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function CrossFader({ track1, track2 }) {
-  const [position, setPosition] = useState(50);
-
-  const crossfade = () => {
-    if (track1 && track1.sound && track2 && track2.sound) {
-      const volume1 = 1 - position / 100;
-      const volume2 = position / 100;
-      track1.sound.volume(volume1);
-      track2.sound.volume(volume2);
-    }
+  const handleCrossFade = (event) => {
+    const value = event.target.value;
+    track1.sound.volume(1 - value / 100);
+    track2.sound.volume(value / 100);
   };
 
-  useEffect(() => {
-    crossfade();
-  }, [position]);
-
   return (
-    <div className="crossfader">
-      <label>Crossfader</label>
+    <div className="crossfader-container">
       <input
         type="range"
         min="0"
         max="100"
-        value={position}
-        onChange={(e) => setPosition(e.target.value)}
+        defaultValue="50"
+        className="crossfader-slider"
+        onChange={handleCrossFade}
       />
     </div>
   );
