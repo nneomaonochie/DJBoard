65d1422462c772d7ef4ea77ec87c3a130278ae65
jaimie-r <jaimie.ren@gmail.com>
2025-03-30 02:28:29 -0500

    app.js edits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import './components/Play'
+import React from 'react';
+import CrossFader from './components/CrossFader.js';
+import Scratching from './components/Scratching.js';
 
 function App() {
-  return (
-    <div className="App">
+  const track1 = { title: 'Track 1', audioFile: 'track1.mp3' };
+  const track2 = { title: 'Track 2', audioFile: 'track2.mp3' };
 
+  return (
+    <div className="dj-board">
+      <TrackDeck track={track1} />
+      <Waveform audioFile={track1.audioFile} />
+      <TrackDeck track={track2} />
+      <Waveform audioFile={track2.audioFile} />
+      <DJControls />
+      <Crossfader track1={track1} track2={track2} />
+      <Looping track={track1} />
+      <Scratching track={track1} />
     </div>
   );
 }
